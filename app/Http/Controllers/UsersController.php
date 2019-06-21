399c36257692ba464d5670821a39268f95fc313f
dengao <2916012032@qq.com>
2019-06-21 17:19:45 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -30,7 +30,19 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程！');
+        return redirect()->route('users.show',$user->id);
+
+
+
+
     }
     //编辑页面
     public function edit()
