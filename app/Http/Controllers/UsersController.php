6a1870c4b9fa04330fda520e72f6da0bdc0cd363
dengao <2916012032@qq.com>
2019-06-21 16:11:20 +0800

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
+    //显示所有用户
+    public function index()
+    {
+
+    }
+    //单个显示
+    public function show(User $user)
+    {
+        return view('users.show',compact('user'));
+    }
+    //新增页面
     public function create()
     {
         return view('users.create');
     }
+    //新增
+    public function store()
+    {
+
+    }
+    //编辑页面
+    public function edit()
+    {
+
+    }
+    //编辑
+    public function update()
+    {
+
+    }
+    //删除
+    public function delete()
+    {
+
+    }
+
+
 }
