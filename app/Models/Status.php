76825a51af9f25b01c730d6d3095dd9ca7ed41a6
vagrant <vagrant@homestead>
2019-06-25 06:39:16 +0000

    发布动态

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content',
+    ];
     public function user()
     {
         return $this->belongsTo(User::class);
