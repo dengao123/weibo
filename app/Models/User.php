2b4cbee46b1152a1be17227022290677cca44b98
vagrant <vagrant@homestead>
2019-06-25 13:13:49 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Models\Status;
 
 class User extends Authenticatable
 {
@@ -52,7 +53,11 @@ class User extends Authenticatable
     //获取所有微博
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at','desc');
+        $user_ids = $this->followings->pluck('id')->toArray();;
+        array_push($user_ids,$this->id);
+        return Status::whereIn('user_id',$user_ids)
+                               ->with('user')
+                               ->orderBy('created_at','desc');
     }
 
     //获取粉丝
